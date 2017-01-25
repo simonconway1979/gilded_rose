9287b0c684544cab9f6572b7867bd81f1f07a410
Simon <simon.conway@condaal.com>
2017-01-25 10:02:04 +0000

    Uncommented tests for Brie

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -31,33 +31,32 @@ describe("Guilded rose", function() {
 
   });
 
+  describe("Updating Brie", function() {
 
-  // describe("Updating Brie", function() {
-  //
-  //   it("Update reduces brie sell in by 1 and increases quality by 1", function() {
-  //     items = [ new Item("Aged Brie", 10, 10) ];
-  //     update_quality();
-  //     expect(items[0].name).toEqual("Aged Brie");
-  //     expect(items[0].sell_in).toEqual(9);
-  //     expect(items[0].quality).toEqual(11);
-  //   });
-  //
-  //   it("when quality is at 50, update will reduce sell in by -1 and quality remains at 50", function() {
-  //     items = [ new Item("Aged Brie", 10, 50) ];
-  //     update_quality();
-  //     expect(items[0].name).toEqual("Aged Brie");
-  //     expect(items[0].sell_in).toEqual(9);
-  //     expect(items[0].quality).toEqual(50);
-  //   });
-  //
-  //   it("when sell in is negative, update reduces sell in by 1 and increases quality by 2", function() {
-  //     items = [ new Item("Aged Brie", -1, 10) ];
-  //     update_quality();
-  //     expect(items[0].name).toEqual("Aged Brie");
-  //     expect(items[0].sell_in).toEqual(-2);
-  //     expect(items[0].quality).toEqual(12);
-  //   });
-  // });
+    it("Update reduces brie sell in by 1 and increases quality by 1", function() {
+      items = [ new Item("Aged Brie", 10, 10) ];
+      update_quality();
+      expect(items[0].name).toEqual("Aged Brie");
+      expect(items[0].sell_in).toEqual(9);
+      expect(items[0].quality).toEqual(11);
+    });
+
+    it("when quality is at 50, update will reduce sell in by -1 and quality remains at 50", function() {
+      items = [ new Item("Aged Brie", 10, 50) ];
+      update_quality();
+      expect(items[0].name).toEqual("Aged Brie");
+      expect(items[0].sell_in).toEqual(9);
+      expect(items[0].quality).toEqual(50);
+    });
+
+    it("when sell in is negative, update reduces sell in by 1 and increases quality by 2", function() {
+      items = [ new Item("Aged Brie", -1, 10) ];
+      update_quality();
+      expect(items[0].name).toEqual("Aged Brie");
+      expect(items[0].sell_in).toEqual(-2);
+      expect(items[0].quality).toEqual(12);
+    });
+  });
   //
   // describe("Updating Sulfuras, Hand of Ragnaros", function() {
   //
