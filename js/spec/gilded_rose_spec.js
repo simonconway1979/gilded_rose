48819bfe564169a30c08bb2c1eb7181c6d614b60
Simon <simon.conway@condaal.com>
2017-01-25 10:44:31 +0000

    Repeated all tests with runUpdate function rather than individual functions

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -136,5 +136,70 @@ describe("Guilded rose", function() {
   //
   // });
 
+  describe("Updating Standard Item", function() {
+
+    it("On update, it should reduce a standard items sell in by 1 and quality by 1", function() {
+      items = [ new Item("Standard", 10, 10) ];
+      update = new Update()
+      update.runUpdate();
+      expect(items[0].name).toEqual("Standard");
+      expect(items[0].sell_in).toEqual(9);
+      expect(items[0].quality).toEqual(9);
+    });
+
+    it("when sell in date is negative, sell in reduces by -1 and quality remains at -2", function() {
+      items = [ new Item("Standard", -1, 10) ];
+      update = new Update()
+      update.runUpdate();
+      expect(items[0].name).toEqual("Standard");
+      expect(items[0].sell_in).toEqual(-2);
+      expect(items[0].quality).toEqual(8);
+    });
+
+  });
+
+
+  describe ('Update quality should set quality to within the allowed range (0-50)', function () {
+
+    it("when quality is negative, the updateQuality method resets to 0", function() {
+      items = [ new Item("Standard", 0, -1)];
+      update = new Update()
+      update.runUpdate();
+      expect(items[0].name).toEqual("Standard");
+      expect(items[0].sell_in).toEqual(0);
+      expect(items[0].quality).toEqual(0);
+    });
+
+    it("when quality is over 50, the updateQuality method resets to 50", function() {
+      items = [ new Item("Standard", 0, 51)];
+      update = new Update()
+      update.runUpdate();
+      expect(items[0].name).toEqual("Standard");
+      expect(items[0].sell_in).toEqual(0);
+      expect(items[0].quality).toEqual(50);
+    });
+  });
+
+
+  describe("Updating Brie", function() {
+
+    it("Update reduces brie sell in by 1 and increases quality by 1", function() {
+      items = [ new Item("Aged brie", 10, 10) ];
+      update.runUpdate();
+      expect(items[0].name).toEqual("Aged brie");
+      expect(items[0].sell_in).toEqual(9);
+      expect(items[0].quality).toEqual(11);
+    });
+
+    it("when sell in is negative, update reduces sell in by 1 and increases quality by 2", function() {
+      items = [ new Item("Aged brie", -1, 10) ];
+      update.runUpdate();
+      expect(items[0].name).toEqual("Aged brie");
+      expect(items[0].sell_in).toEqual(-2);
+      expect(items[0].quality).toEqual(12);
+    });
+  });
+
+
 
 });
